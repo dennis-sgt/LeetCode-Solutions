8a0f64af847bc556ab280c393ead92aab0b9fbb2
Dennis <52104140+itzd3nn1s@users.noreply.github.com>
2022-08-22 22:35:58 +0200

    Update TwoSum.java

--- Problems/TwoSum.java
+++ Problems/TwoSum.java
@@ -1,10 +1,12 @@
+//=====
 package Problems;
 
 import java.util.HashMap;
 import java.util.Map;
+//=====
 
 public class TwoSum {
-    public static int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++) {
             if(map.containsKey(target - nums[i])) {
