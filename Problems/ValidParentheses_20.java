411bb2504c16c89b7fb7c4a74deb6de85aa4c46d
d3nn1s <dennis_siegert@web.de>
2022-08-31 18:53:08 +0200

    Added ValidParentheses_20

---
+++ Problems/ValidParentheses_20.java
@@ -0,0 +1,22 @@
+public class ValidParentheses_20 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (stack.empty()) {
+                return false;
+            } else if (c == ')' && stack.pop() != '(') {
+                return false;
+            } else if (c == '}' && stack.pop() != '{') {
+                return false;
+            } else if (c == ']' && stack.pop() != '[') {
+                return false;
+            }
+        }
+
+        return stack.empty();
+    }
+}
