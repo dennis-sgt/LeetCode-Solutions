b3cabcfc96a9e23727666ef013394edb979a3842
d3nn1s <dennis_siegert@web.de>
2022-09-04 15:59:57 +0200

    Added ConcatenationOfArray_1929

---
+++ Problems/ConcatenationOfArray_1929.java
@@ -0,0 +1,12 @@
+public class ConcatenationOfArray_1929 {
+    public int[] getConcatenation(int[] nums) {
+        int[] arr = new int[nums.length * 2];
+
+        for(int i = 0; i < nums.length; i++) {
+            arr[i] = nums[i];
+            arr[i + nums.length] = nums[i];
+        }
+
+        return arr;
+    }
+}
