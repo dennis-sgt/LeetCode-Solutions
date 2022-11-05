bfccc8181ed25bacb959e347ae536a56bdb85469
Dennis <52104140+dennis-sgt@users.noreply.github.com>
2022-11-05 16:31:45 +0100

    Update CountAsterisks_2315.py

--- Problems/CountAsterisks_2315.py
+++ Problems/CountAsterisks_2315.py
@@ -1,8 +1,3 @@
 class Solution:
     def countAsterisks(self, s: str) -> int:
-        counter = 0
-        split = s.split("|")
-        print(split)
-        for i in range(0, len(split), 2):
-            counter += split[i].count("*")
-        return counter
+        return sum(list(s.split("|")[i].count("*") for i in range(0, len(s.split("|")), 2)))
