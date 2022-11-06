df36ff1606d21597d98ae588f3fcb3b19feeedd5
Dennis <52104140+dennis-sgt@users.noreply.github.com>
2022-11-06 15:07:58 +0100

    Update NumberOfStringsThatAppearAsSubstringsInWord_1967.py

--- Problems/NumberOfStringsThatAppearAsSubstringsInWord_1967.py
+++ Problems/NumberOfStringsThatAppearAsSubstringsInWord_1967.py
@@ -1,7 +1,3 @@
 class Solution:
     def numOfStrings(self, patterns: list[str], word: str) -> int:
-        output = 0
-        for i in range(len(patterns)):
-            if patterns[i] in word:
-                output += 1
-        return output
+        return sum(list(1 for i in range(len(patterns)) if patterns[i] in word))
